f7bc4fdc12280e45b0dd21be9209d0503649577c
Арсений Панфилов <leepstv@gmail.com>
2024-11-03 16:21:49 +0500

    Add files via upload

---
+++ cypress.config.js
@@ -0,0 +1,19 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    env:{
+      login:"german@dolnikov.ru",
+      password:"iLoveqastudio1"
+    },
+    viewportHeight: 1080,
+    viewportWidth: 1920,
+    blockHosts: ["*mc.yandex.ru"],
+    baseUrl:"https://login.qa.studio"
+  },
+});
+
+// Все параметры конфига: https://docs.cypress.io/guides/references/configuration
